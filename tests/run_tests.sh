045440eba0bea51f20720939752bb4cc13456462
eliphatfs <flandre@scarletx.cn>
2023-03-15 01:14:07 +0800

    Fix CI.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,6 +1,6 @@
 source basics.sh
 
 sudo useradd -m testuser
-sudo -iu testuser bash -c "cd $(pwd) && pwd && ls && bash setup.sh"
+sudo -iu testuser bash -c "cd $(pwd) && bash set_up.sh"
 
 sudo -iu testuser bash -c "cd $(pwd) && bash srcml.sh"
