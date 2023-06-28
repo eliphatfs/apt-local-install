3b3b55526601e20c30b679adeb59ddb17a608d9d
eliphatfs <flandre@scarletx.cn>
2023-06-28 18:41:20 +0800

    Bump CI Ubuntu version.

--- tests/srcml.sh
+++ tests/srcml.sh
@@ -1,6 +1,6 @@
 source basics.sh
 
 cd ~
-wget https://github.com/srcML/srcMLReleases/raw/main/srcml_1.0.0-1_ubuntu18.04.deb
-aptli srcml_1.0.0-1_ubuntu18.04.deb
+wget https://github.com/srcML/srcMLReleases/raw/main/srcml_1.0.0-1_ubuntu20.04.deb
+aptli srcml_1.0.0-1_ubuntu20.04.deb
 srcml -V
